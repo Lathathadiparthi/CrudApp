3d2f794b25c3a42d7eada57047cc420ee15a1803
Lathathadiparthi <105854631+Lathathadiparthi@users.noreply.github.com>
2022-11-07 23:00:36 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import{ HttpClientModule} from '@angular/common/http';
+import {FormsModule } from '@angular/forms';
+import { AddUserComponent } from './add-user/add-user.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
